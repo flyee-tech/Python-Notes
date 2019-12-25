9a045d5e5191a992c4846f1c333b931109e67d9c
peierlong <peielcn@gmail.com>
2019-12-25 11:47:29 +0800

    feat: update repository

---
+++ pd/version_test.py
@@ -0,0 +1,16 @@
+# 导入所有需要的库
+
+# 导入一个库中指定函数的做法
+from pandas import DataFrame, read_csv
+
+# 导入整个库的做法
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import matplotlib
+
+
+print('Python version ' + sys.version)
+print('Pandas version ' + pd.__version__)
+print('Matplotlib version ' + matplotlib.__version__)
+
